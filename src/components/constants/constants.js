14970c30a9f8a7eadc2687431ba3ad6105f109ea
Obaid Ur Rehman <obaidkhan972@gmail.com>
2022-02-18 12:45:57 +0500

    Connected to heroku back-end.

--- src/components/constants/constants.js
+++ src/components/constants/constants.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL : "http://localhost:4000",
+    // baseURL : "http://localhost:4000",
+    baseURL : "https://tinder09.herokuapp.com",
 });
 export default instance;
